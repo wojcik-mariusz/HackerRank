a2ac2de286d067bf5c26885ddf3f732680ba90f1
MFireIt <wojcikk.mariusz@gmail.com>
2022-08-12 22:49:57 +0200

    Add count_substring functionality

--- Python/Strings/Find_a_string.py
+++ Python/Strings/Find_a_string.py
@@ -1,8 +1,18 @@
-def count_substring(string, sub_string):
-    return
+def count_substring(text: str, sub_string: str):
+    counter: int = 0
+    len_of_text: int = len(text)
+    len_of_sub_string: int = len(sub_string)
+    index = 0
+    for i in list(text):
+        if index < (len_of_text - len_of_sub_string + 1):
+            success = text.find(sub_string, index, index + len_of_sub_string)
+            if success != -1:
+                counter += 1
+            index += 1
+    return counter
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     string = input().strip()
     sub_string = input().strip()
 
