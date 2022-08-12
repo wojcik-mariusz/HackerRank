91ec97d274ed484fbe51c311e4ed04f81c70d2d5
MFireIt <wojcikk.mariusz@gmail.com>
2022-08-12 23:17:27 +0200

    Add solution

--- Python/Strings/String_Validators.py
+++ Python/Strings/String_Validators.py
@@ -1,2 +1,65 @@
-if __name__ == '__main__':
+from typing import NoReturn
+
+
+def is_any_alphanum(s: str) -> NoReturn:
+    flag = False
+    for i in list(s):
+        if i.isalnum():
+            flag = True
+            break
+        else:
+            continue
+    print(flag)
+
+
+def is_any_alphabetical(s: str) -> NoReturn:
+    flag = False
+    for i in list(s):
+        if i.isalpha():
+            flag = True
+            break
+        else:
+            continue
+    print(flag)
+
+
+def is_any_digit(s: str) -> NoReturn:
+    flag = False
+    for i in list(s):
+        if i.isdigit():
+            flag = True
+            break
+        else:
+            continue
+    print(flag)
+
+
+def is_any_lowercase(s: str) -> NoReturn:
+    flag = False
+    for i in list(s):
+        if i.islower():
+            flag = True
+            break
+        else:
+            continue
+    print(flag)
+
+
+def is_any_uppercase(s: str) -> NoReturn:
+    flag = False
+    for i in list(s):
+        if i.isupper():
+            flag = True
+            break
+        else:
+            continue
+    print(flag)
+
+
+if __name__ == "__main__":
     s = input()
+    is_any_alphanum(s)
+    is_any_alphabetical(s)
+    is_any_digit(s)
+    is_any_lowercase(s)
+    is_any_uppercase(s)
