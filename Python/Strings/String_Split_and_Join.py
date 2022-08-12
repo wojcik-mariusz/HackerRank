ce939075280b5f5d130f766abb34ff92914f366b
MFireIt <wojcikk.mariusz@gmail.com>
2022-08-12 18:04:26 +0200

    Add solution

--- Python/Strings/String_Split_and_Join.py
+++ Python/Strings/String_Split_and_Join.py
@@ -1,4 +1,16 @@
-def split_and_join(line):
-    # write your code here
+from typing import List, NoReturn
+
+
+def split_and_join(line: str) -> str:
+    text_to_split: List[str] = line.split(" ")
+    splitted_text: str = "-".join(word for word in text_to_split)
+    return splitted_text
+
+
+def print_to_console(arg:str) -> NoReturn:
+    print(arg)
+
 
 if __name__ == '__main__':
+    text = input()
+    print_to_console(split_and_join(text))
