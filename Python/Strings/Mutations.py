8f59179c3ef10915c8cd77c91a860e8dfa6e1102
MFireIt <wojcikk.mariusz@gmail.com>
2022-08-12 18:27:29 +0200

    Add solution

--- Python/Strings/Mutations.py
+++ Python/Strings/Mutations.py
@@ -1,7 +1,11 @@
-def mutate_string(string, position, character):
-    return
+def mutate_string(text: str, position: int, character: str) -> str:
+    text = list(text)
+    text[position] = character
+    conv_text = "".join(text)
+    return conv_text
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     s = input()
     i, c = input().split()
     s_new = mutate_string(s, int(i), c)
