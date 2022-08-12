e18967586f47858126248a4d4204f90c1d186e0a
MFireIt <wojcikk.mariusz@gmail.com>
2022-08-12 23:21:18 +0200

    refactor, use any() function

--- Python/Strings/String_Validators.py
+++ Python/Strings/String_Validators.py
@@ -1,65 +1,7 @@
-from typing import NoReturn
-
-
-def is_any_alphanum(s: str) -> NoReturn:
-    flag = False
-    for i in list(s):
-        if i.isalnum():
-            flag = True
-            break
-        else:
-            continue
-    print(flag)
-
-
-def is_any_alphabetical(s: str) -> NoReturn:
-    flag = False
-    for i in list(s):
-        if i.isalpha():
-            flag = True
-            break
-        else:
-            continue
-    print(flag)
-
-
-def is_any_digit(s: str) -> NoReturn:
-    flag = False
-    for i in list(s):
-        if i.isdigit():
-            flag = True
-            break
-        else:
-            continue
-    print(flag)
-
-
-def is_any_lowercase(s: str) -> NoReturn:
-    flag = False
-    for i in list(s):
-        if i.islower():
-            flag = True
-            break
-        else:
-            continue
-    print(flag)
-
-
-def is_any_uppercase(s: str) -> NoReturn:
-    flag = False
-    for i in list(s):
-        if i.isupper():
-            flag = True
-            break
-        else:
-            continue
-    print(flag)
-
-
 if __name__ == "__main__":
     s = input()
-    is_any_alphanum(s)
-    is_any_alphabetical(s)
-    is_any_digit(s)
-    is_any_lowercase(s)
-    is_any_uppercase(s)
+    print(any([char.isalnum() for char in s]))
+    print(any([char.isalpha() for char in s]))
+    print(any([char.isdigit() for char in s]))
+    print(any([char.islower() for char in s]))
+    print(any([char.isupper() for char in s]))
