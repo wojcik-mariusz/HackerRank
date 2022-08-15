fd19962fd3562c1fbfa43d0539ec684de479d8fa
Mariusz W <wojcikk.mariusz@gmail.com>
2022-08-15 02:09:58 +0200

    add solution

--- Python/Strings/Designer_Door_Mat.py
+++ Python/Strings/Designer_Door_Mat.py
@@ -1,1 +1,15 @@
+N, M = map(int, input().split(" "))
+pattern_height = N
+pattern_width = M
+welcome_str = "WELCOME"
+welcome_pattern = welcome_str.center(pattern_width, "-")
+up_down_pattern = ".|."
+for i in range(((pattern_height - 1) // 2)):
+    x = up_down_pattern * (i * 2 + 1)
+    print(x.center(M, "-"))
 
+print(welcome_pattern)
+
+for i in range(((pattern_height - 1) // 2), 0, -1):
+    x = up_down_pattern * (i * 2 - 1)
+    print(x.center(M, "-"))
