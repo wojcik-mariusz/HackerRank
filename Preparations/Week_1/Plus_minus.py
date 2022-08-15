8dcaeee7dbc3f4f6a979c6714e3c42d3475a85e3
Mariusz W <wojcikk.mariusz@gmail.com>
2022-08-15 22:27:04 +0200

    Rename variables to avoid shadow-named.

--- Preparations/Week_1/Plus_minus.py
+++ Preparations/Week_1/Plus_minus.py
@@ -3,14 +3,14 @@ from typing import List, NoReturn
 #
 # Complete the 'plusMinus' function below.
 #
-# The function accepts INTEGER_ARRAY arr as parameter.
+# The function accepts INTEGER_ARRAY arr_list as parameter.
 #
 
 
-def plusMinus(arr: List[int]) -> NoReturn:
-    positive_ints = list(filter(lambda x: x > 0, arr))
-    negative_ints = list(filter(lambda x: x < 0, arr))
-    zeros = list(filter(lambda x: x == 0, arr))
+def plus_minus(iterable: List[int]) -> NoReturn:
+    positive_ints = list(filter(lambda x: x > 0, iterable))
+    negative_ints = list(filter(lambda x: x < 0, iterable))
+    zeros = list(filter(lambda x: x == 0, iterable))
     positive_ints_ratio: float = len(positive_ints) / n
     negative_ints_ratio: float = len(negative_ints) / n
     zeros_ratio: float = len(zeros) / n
@@ -25,6 +25,6 @@ def plusMinus(arr: List[int]) -> NoReturn:
 if __name__ == "__main__":
     n = int(input().strip())
 
-    arr = list(map(int, input().rstrip().split()))
+    arr_list = list(map(int, input().rstrip().split()))
 
-    plusMinus(arr)
+    plus_minus(iterable=arr_list)
