c8d29afe59998194b74b8e4e0cba7371019220ff
MFireIt <wojcikk.mariusz@gmail.com>
2022-08-24 15:28:40 +0200

    Add solution

--- 3m/Time_Conversion.py
+++ 3m/Time_Conversion.py
@@ -1,4 +1,3 @@
-
 import math
 import os
 import random
@@ -10,11 +9,30 @@ import sys
 #
 # The function is expected to return a STRING.
 # The function accepts STRING s as parameter.
-#
 
-def timeConversion(s):
-    pass
-if __name__ == '__main__':
+
+def is_am(input_time: str) -> bool:
+    return True if input_time.endswith("AM") else False
+
+
+def timeConversion(s: str) -> str:
+    hour: str = s[:2]
+    minute_and_second: str = s[2:-2]
+    if is_am(s):
+        if hour == "12":
+            hour = "00"
+        converted_time = hour + minute_and_second
+
+    else:
+        if hour == "12":
+            hour: str = "12"
+        else:
+            hour = str(int(hour) + 12)
+        converted_time = hour + minute_and_second
+    return converted_time
+
+
+if __name__ == "__main__":
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     s = input()
