1daaaea11b32a91f37b1d0c32f9905121e0954cb
MFireIt <wojcikk.mariusz@gmail.com>
2022-08-24 15:46:55 +0200

    Add solution

--- 3m/Breaking_the_Records.py
+++ 3m/Breaking_the_Records.py
@@ -1,10 +1,9 @@
-#!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
+from typing import List
 
 #
 # Complete the 'breakingRecords' function below.
@@ -13,11 +12,25 @@ import sys
 # The function accepts INTEGER_ARRAY scores as parameter.
 #
 
-def breakingRecords(scores):
-    # Write your code here
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+def breakingRecords(scores) -> List[int]:
+    break_min_counter: int = 0
+    break_max_counter: int = 0
+    min_score = List[0]
+    max_score = List[0]
+    for actual_score in List[1:]:
+        if actual_score < min_score:
+            min_score = actual_score
+            break_min_counter += 1
+        elif actual_score > max_score:
+            max_score = actual_score
+            break_max_counter += 1
+    result = [break_max_counter, break_min_counter]
+    return result
+
+
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
 
     n = int(input().strip())
 
@@ -25,7 +38,7 @@ if __name__ == '__main__':
 
     result = breakingRecords(scores)
 
-    fptr.write(' '.join(map(str, result)))
-    fptr.write('\n')
+    fptr.write(" ".join(map(str, result)))
+    fptr.write("\n")
 
     fptr.close()
