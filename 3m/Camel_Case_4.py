04f5dc0cc5b7582f71de39a1c3be87c7e1db04b3
MFireIt <wojcikk.mariusz@gmail.com>
2022-08-24 19:24:35 +0200

    add functionality to create variable name if type output is "V"

--- 3m/Camel_Case_4.py
+++ 3m/Camel_Case_4.py
@@ -9,9 +9,12 @@ type_output = params[1]
 name = params[2]
 
 
-def create_var_name(operation: str, name: str):
+def create_var_name(operation: str, name: str, type_output: str):
     if operation == "C":
-        var_name = "".join(x for x in name)
+        if type_output == "V":
+            name: List[str] = name.split()
+            var_name: str = name[0] + "".join(x.title() for x in name[1:])
+
     else:
         var_name = ""
         for i in name:
@@ -21,5 +24,8 @@ def create_var_name(operation: str, name: str):
             else:
                 var_name += i
         var_name = var_name.replace("*", " ").lstrip()
+    print(var_name)
     return var_name
+
+create_var_name(operation=operation, name=name, type_output=type_output)
 
