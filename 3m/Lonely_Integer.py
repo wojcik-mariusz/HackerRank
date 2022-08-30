6bf5c2dad20eb83b42ec3adaaa0b7b97e9467f86
MFireIt <wojcikk.mariusz@gmail.com>
2022-08-30 18:50:45 +0200

    add solution

--- 3m/Lonely_Integer.py
+++ 3m/Lonely_Integer.py
@@ -6,6 +6,8 @@ import random
 import re
 import sys
 
+from typing import List
+
 #
 # Complete the 'lonelyinteger' function below.
 #
@@ -13,8 +15,14 @@ import sys
 # The function accepts INTEGER_ARRAY a as parameter.
 #
 
-def lonelyinteger(a):
-    # Write your code here
+
+def lonelyinteger(a: List[int]) -> int:
+    for element in a:
+        if a.count(element) == 1:
+            return element
+
+
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -25,6 +33,5 @@ if __name__ == '__main__':
 
     result = lonelyinteger(a)
 
-    fptr.write(str(result) + '\n')
 
     fptr.close()
